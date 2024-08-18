c15c251b765e3af17c72c7832a29c351def6a74c
Rohitk131 <rohitkumar13103@gmail.com>
2024-08-18 20:48:51 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+app.get('/', (req,res)=>{
+    res.send("Hello")
+})
+app.listen(3000, ()=>{
+    console.log("Server started on port: 3000")
+});
+
+
+module.exports = app; 
